a4a99b3bedc7bcd5cd32cfe301bd2b3fe4dc9f5b
JP Rodman <jpr@rentrak.com>
2017-09-27 18:40:27 -0700

    Add option to specify the user validation method.
    And convert it to return boolean.

--- flask_praetorian/constants.py
+++ flask_praetorian/constants.py
@@ -8,6 +8,7 @@ DEFAULT_JWT_ACCESS_LIFESPAN = dict(minutes=15)
 DEFAULT_JWT_REFRESH_LIFESPAN = dict(days=30)
 DEFAULT_JWT_ALGORITHM = 'HS256'
 DEFAULT_JWT_ALLOWED_ALGORITHMS = ['HS256']
+DEFAULT_USER_CLASS_VALIDATION_METHOD = 'is_valid'
 
 # 1M days seems reasonable. If this code is being used in 3000 years...welp
 VITAM_AETERNUM = pendulum.Interval(days=1000000)
