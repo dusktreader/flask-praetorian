f73ab67261c978fc8f975703676edd16c83c428a
Rob Dailey <rob@wargam.es>
2018-11-13 17:06:43 -0500

    146#discussion_r233146306 Formatting cleanup

--- flask_praetorian/constants.py
+++ flask_praetorian/constants.py
@@ -16,9 +16,10 @@ DEFAULT_CONFIRMATION_SUBJECT = 'You still need to confirm your registration'
 DEFAULT_HASH_AUTOUPDATE = False
 DEFAULT_HASH_AUTOTEST = False
 DEFAULT_HASH_SCHEME = 'pbkdf2_sha512'
-DEFAULT_HASH_ALLOWED_SCHEMES = ['pbkdf2_sha512', 'sha256_crypt',
-                                'sha512_crypt', 'bcrypt', 'argon2',
-                                'bcrypt_sha256']
+DEFAULT_HASH_ALLOWED_SCHEMES = [
+    'pbkdf2_sha512', 'sha256_crypt', 'sha512_crypt', 'bcrypt', 'argon2',
+    'bcrypt_sha256',
+]
 DEFAULT_HASH_DEPRECATED_SCHEMES = []
 
 RESERVED_CLAIMS = {'iat', 'exp', 'rf_exp', 'jti', 'id', 'rls'}
