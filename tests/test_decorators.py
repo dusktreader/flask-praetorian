4bae98c2465d541cee239867f6a72b065c60ac2d
kreyoo <zunder325@gmail.com>
2021-11-09 12:32:44 +0100

    make tests compatible with pyjwt 2.2

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,4 +1,4 @@
-import pendulum
+import datetime
 import pytest
 import freezegun
 
@@ -6,7 +6,6 @@ from flask_praetorian.exceptions import MissingRoleError
 
 
 class TestPraetorianDecorators:
-
     @pytest.fixture(autouse=True)
     def setup(self, db, user_class, default_guard):
         """
@@ -14,28 +13,28 @@ class TestPraetorianDecorators:
         decorators thoroughly
         """
         self.the_dude = user_class(
-            username='TheDude',
-            password=default_guard.hash_password('abides'),
+            username="TheDude",
+            password=default_guard.hash_password("abides"),
         )
         self.walter = user_class(
-            username='Walter',
-            password=default_guard.hash_password('calmerthanyouare'),
-            roles='admin'
+            username="Walter",
+            password=default_guard.hash_password("calmerthanyouare"),
+            roles="admin",
         )
         self.donnie = user_class(
-            username='Donnie',
-            password=default_guard.hash_password('iamthewalrus'),
-            roles='operator'
+            username="Donnie",
+            password=default_guard.hash_password("iamthewalrus"),
+            roles="operator",
         )
         self.maude = user_class(
-            username='Maude',
-            password=default_guard.hash_password('andthorough'),
-            roles='operator,admin'
+            username="Maude",
+            password=default_guard.hash_password("andthorough"),
+            roles="operator,admin",
         )
         self.jesus = user_class(
-            username='Jesus',
-            password=default_guard.hash_password('hecanroll'),
-            roles='admin,god'
+            username="Jesus",
+            password=default_guard.hash_password("hecanroll"),
+            roles="admin,god",
         )
 
         db.session.add(self.the_dude)
@@ -54,29 +53,25 @@ class TestPraetorianDecorators:
 
         # Token is not in header
         response = client.get(
-            '/kinda_protected',
+            "/kinda_protected",
             headers={},
         )
         assert response.status_code == 200
-        assert (
-            "success"
-            in response.json['message']
-        )
-        assert response.json['user'] is None
+        assert "success" in response.json["message"]
+        assert response.json["user"] is None
 
         # Token is present and valid
-        moment = pendulum.parse('2017-05-24 10:38:45')
+        moment = datetime.datetime(
+            2017, 5, 24, 10, 38, 45, tzinfo=datetime.timezone.utc
+        )
         with freezegun.freeze_time(moment):
             response = client.get(
-                '/kinda_protected',
+                "/kinda_protected",
                 headers=default_guard.pack_header_for_user(self.the_dude),
             )
             assert response.status_code == 200
-            assert (
-                "success"
-                in response.json['message']
-            )
-            assert response.json['user'] == self.the_dude.username
+            assert "success" in response.json["message"]
+            assert response.json["user"] == self.the_dude.username
 
     def test_auth_required(self, client, default_guard):
         """
@@ -88,51 +83,46 @@ class TestPraetorianDecorators:
 
         # Token is not in header
         response = client.get(
-            '/protected',
+            "/protected",
             headers={},
         )
-        assert (
-            "JWT token not found"
-            in response.json['message']
-        )
+        assert "JWT token not found" in response.json["message"]
         assert response.status_code == 401
 
         # Token has invalid structure
         response = client.get(
-            '/protected',
-            headers={'Authorization': 'bad_structure iamatoken'},
+            "/protected",
+            headers={"Authorization": "bad_structure iamatoken"},
         )
-        assert (
-            "JWT header structure is invalid"
-            in response.json['message']
-        )
+        assert "JWT header structure is invalid" in response.json["message"]
         assert response.status_code == 401
 
         # Token is expired
-        moment = pendulum.parse('2017-05-24 10:18:45')
+        moment = datetime.datetime(
+            2017, 5, 24, 10, 18, 45, tzinfo=datetime.timezone.utc
+        )
         with freezegun.freeze_time(moment):
             headers = default_guard.pack_header_for_user(self.the_dude)
         moment = (
-            moment +
-            default_guard.access_lifespan +
-            pendulum.Duration(seconds=1)
+            moment
+            + default_guard.access_lifespan
+            + datetime.timedelta(seconds=1)
         )
         with freezegun.freeze_time(moment):
             response = client.get(
-                '/protected',
+                "/protected",
                 headers=headers,
             )
             assert response.status_code == 401
-            assert (
-                "access permission has expired"
-                in response.json['message']
-            )
+            assert "access permission has expired" in response.json["message"]
 
         # Token is present and valid
-        moment = pendulum.parse('2017-05-24 10:38:45')
+        moment = datetime.datetime(
+            2017, 5, 24, 10, 38, 45, tzinfo=datetime.timezone.utc
+        )
         with freezegun.freeze_time(moment):
             response = client.get(
-                '/protected',
+                "/protected",
                 headers=default_guard.pack_header_for_user(self.the_dude),
             )
             assert response.status_code == 200
@@ -149,55 +139,55 @@ class TestPraetorianDecorators:
 
         # Lacks one of one required roles
         response = client.get(
-            '/protected_admin_required',
+            "/protected_admin_required",
             headers=default_guard.pack_header_for_user(self.the_dude),
         )
         assert response.status_code == 403
         assert (
             "This endpoint requires all the following roles"
-            in response.json['message']
+            in response.json["message"]
         )
 
         # Has one of one required roles
         response = client.get(
-            '/protected_admin_required',
+            "/protected_admin_required",
             headers=default_guard.pack_header_for_user(self.walter),
         )
         assert response.status_code == 200
 
         # Lacks one of two required roles
         response = client.get(
-            '/protected_admin_and_operator_required',
+            "/protected_admin_and_operator_required",
             headers=default_guard.pack_header_for_user(self.walter),
         )
         assert response.status_code == 403
-        assert MissingRoleError.__name__ in response.json['error']
+        assert MissingRoleError.__name__ in response.json["error"]
         assert (
             "This endpoint requires all the following roles"
-            in response.json['message']
+            in response.json["message"]
         )
 
         # Has two of two required roles
         response = client.get(
-            '/protected_admin_and_operator_required',
+            "/protected_admin_and_operator_required",
             headers=default_guard.pack_header_for_user(self.maude),
         )
         assert response.status_code == 200
 
         response = client.get(
-            '/undecorated_admin_required',
+            "/undecorated_admin_required",
             headers=default_guard.pack_header_for_user(self.maude),
         )
         assert response.status_code == 200
 
         response = client.get(
-            '/undecorated_admin_accepted',
+            "/undecorated_admin_accepted",
             headers=default_guard.pack_header_for_user(self.maude),
         )
         assert response.status_code == 200
 
         response = client.get(
-            '/reversed_decorators',
+            "/reversed_decorators",
             headers=default_guard.pack_header_for_user(self.maude),
         )
         assert response.status_code == 200
@@ -210,42 +200,42 @@ class TestPraetorianDecorators:
         supplied, a 401 error occurs with an informative error message.
         """
         response = client.get(
-            '/protected',
+            "/protected",
             headers=default_guard.pack_header_for_user(self.the_dude),
         )
         assert response.status_code == 200
 
         response = client.get(
-            '/protected_admin_and_operator_accepted',
+            "/protected_admin_and_operator_accepted",
             headers=default_guard.pack_header_for_user(self.the_dude),
         )
         assert response.status_code == 403
-        assert MissingRoleError.__name__ in response.json['error']
+        assert MissingRoleError.__name__ in response.json["error"]
         assert (
             "This endpoint requires one of the following roles"
-            in response.json['message']
+            in response.json["message"]
         )
 
         response = client.get(
-            '/protected_admin_and_operator_accepted',
+            "/protected_admin_and_operator_accepted",
             headers=default_guard.pack_header_for_user(self.walter),
         )
         assert response.status_code == 200
 
         response = client.get(
-            '/protected_admin_and_operator_accepted',
+            "/protected_admin_and_operator_accepted",
             headers=default_guard.pack_header_for_user(self.donnie),
         )
         assert response.status_code == 200
 
         response = client.get(
-            '/protected_admin_and_operator_accepted',
+            "/protected_admin_and_operator_accepted",
             headers=default_guard.pack_header_for_user(self.maude),
         )
         assert response.status_code == 200
 
         response = client.get(
-            '/protected_admin_and_operator_accepted',
+            "/protected_admin_and_operator_accepted",
             headers=default_guard.pack_header_for_user(self.jesus),
         )
         assert response.status_code == 200
